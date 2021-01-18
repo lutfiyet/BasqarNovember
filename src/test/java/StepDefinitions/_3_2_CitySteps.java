e8cefd175d8cdfedf6202a4f949c071f2439da01
lutfiyet <ltfytpcdrn@gmail.com>
2021-01-18 14:39:27 +0100

    Initial commit

---
+++ src/test/java/StepDefinitions/_3_2_CitySteps.java
@@ -0,0 +1,37 @@
+package StepDefinitions;
+
+import Pages.DialogContent;
+import Pages.LeftNavigation;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+
+public class _3_2_CitySteps {
+    LeftNavigation leftNavigation=new LeftNavigation();
+    DialogContent dialogContent= new DialogContent();
+
+    @When("^navigate to city page$")
+    public void navigateToCityPage() {
+        leftNavigation.findElementAndClickFunction("setUpGeneral");
+        leftNavigation.findElementAndClickFunction("parameterButton");
+        leftNavigation.findElementAndClickFunction("citiesButton");
+    }
+
+    @Then("^create a city$")
+    public void createACity() {
+        dialogContent.findElementAndClickFunction("addButton");
+        dialogContent.findElementAndClickFunction("countryClick");
+        dialogContent.findElementAndClickFunction("landWahl");
+        dialogContent.findElementAndSendKeysFunction("cityNameInput", "Bischkek");
+        dialogContent.findElementAndClickFunction("saveButton");
+    }
+
+    @Then("^delete a city$")
+    public void deleteACity() throws InterruptedException {
+        dialogContent.findElementAndSendKeysFunction("citySearchInput", "Bischkek");
+        dialogContent.findElementAndClickFunction("searchButton");
+        Thread.sleep(200);
+        dialogContent.findElementAndClickFunction("deleteBtn");
+        dialogContent.findElementAndClickFunction("yesButton");
+
+    }
+}
